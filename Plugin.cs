97a408078d68c91aea157ddc174a3a0a4e4bbacf
anargya-web <132619756+anargya-web@users.noreply.github.com>
2024-03-19 20:10:39 +0700

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,26 @@
+﻿using TerrariaApi.Server;
+
+namespace PluginTemplate
+{
+    [ApiVersion(2, 1)]
+    public partial class Plugin : TerrariaPlugin
+    {
+        public override void Initialize()
+        {
+            // Initialize here
+            // Hooks
+            // ServerApi.Hooks
+            // OTAPI.Hooks
+            // On.Terraria Monomod hooks
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                // Dispose here
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
